13203eae861c9f6150fb548c42e056402d1fef9a
addavriance <61160742+addavriance@users.noreply.github.com>
2024-05-16 01:32:18 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# UpperLowerIndex
- Switch upper and lower index character input
+# KeyIndexSwitcher
+
+KeyIndexSwitcher is a Python script that allows you to switch between different input modes (base, upper indices, and lower indices) by pressing specified function keys (F1 and F4 by default).
+
+This tool is useful for typing mathematical and scientific notations that require superscript or subscript characters.
+
+## Features
+
+- Switch between base, upper indices, and lower indices input modes.
+- Default keys for switching modes: F1 (upper indices) and F4 (lower indices).
+- Easily customizable key mappings.
+
+## Installation
+
+1. **Clone the repository:**
+
+    ```bash
+    git clone https://github.com/addavriance/KeyIndexSwitcher.git
+    cd KeyIndexSwitcher
+    ```
+
+2. **Install the required dependencies:**
+
+    Make sure you have `pynput` installed. If not, you can install it using pip:
+
+    ```bash
+    pip install pynput
+    ```
+
+## Usage
+
+1. **Run the script:**
+
+    ```bash
+    python main.py
+    ```
+
+2. **Switching Modes:**
+    - Press `F1` to switch to upper indices mode.
+    - Press `F4` to switch to lower indices mode.
+    - Press the same key again to switch back to base mode.
+
+3. **Typing:**
+    - In upper indices mode, characters such as `a`, `b`, `c`, etc., will be converted to their superscript equivalents.
+    - In lower indices mode, characters such as `a`, `b`, `c`, etc., will be converted to their subscript equivalents.
+
+## Customization
+
+### Changing Key Mappings
+
+You can change the keys used to switch modes by modifying the constants in `constants.py`:
+
+```python
+# constants.py
+
+from pynput.keyboard import Key
+
+UPPER_KEY = Key.f1  # Change this to your preferred key for upper indices
+LOWER_KEY = Key.f4  # Change this to your preferred key for lower indices
---
+++ main.py
@@ -0,0 +1,79 @@
+from constants import UPPER_INDICES, LOWER_INDICES, UPPER_KEY, LOWER_KEY
+
+from pynput.keyboard import Key, Controller, Listener
+from enum import Enum
+
+
+class IndexMode(Enum):
+    Base = 0,
+    Upper = 1,
+    Lower = 2,
+
+
+class IndexConverter:
+    def __init__(self):
+        self.mode: IndexMode = IndexMode.Base
+
+        self.keyboard = Controller()
+
+        self.upper_key = UPPER_KEY
+        self.lower_key = LOWER_KEY
+
+        self.shortcut_pressed = False
+
+        self.shortcut_keys = [Key.cmd, Key.alt, Key.ctrl, Key.cmd_r, Key.alt_r, Key.ctrl_r, Key.cmd_l, Key.alt_l, Key.ctrl_l, Key.alt_gr]
+
+    def switch_mode(self, key: Key):
+        if key == self.upper_key:
+            self.mode = IndexMode.Upper if self.mode != IndexMode.Upper else IndexMode.Base
+        elif key == self.lower_key:
+            self.mode = IndexMode.Lower if self.mode != IndexMode.Lower else IndexMode.Base
+
+    def on_press(self, key):
+        if key in self.shortcut_keys:
+            self.shortcut_pressed = True
+
+        if self.shortcut_pressed:
+            return
+
+        key in [self.upper_key, self.lower_key] and self.switch_mode(key)
+
+        if self.mode == IndexMode.Upper:
+            self.handle_key_press(key, UPPER_INDICES)
+        elif self.mode == IndexMode.Lower:
+            self.handle_key_press(key, LOWER_INDICES)
+
+    def on_release(self, key):
+        if key in self.shortcut_keys:
+            self.shortcut_pressed = False
+
+    def type_char(self, char):
+        self.keyboard.press(char)
+        self.keyboard.release(char)
+
+    def remove_char(self):
+        self.keyboard.press(Key.backspace)
+        self.keyboard.release(Key.backspace)
+
+    def handle_key_press(self, key, index_mapping):
+        try:
+            char = key.char
+        except AttributeError:
+            return
+
+        if char in index_mapping:
+
+            index_char = index_mapping[char]
+
+            self.remove_char()
+
+            self.type_char(index_char)
+
+
+if __name__ == "__main__":
+    converter = IndexConverter()
+    try:
+        with Listener(on_press=converter.on_press, on_release=converter.on_release) as listener:
+            listener.join()
+    except KeyboardInterrupt:
+        print("Bye!")
